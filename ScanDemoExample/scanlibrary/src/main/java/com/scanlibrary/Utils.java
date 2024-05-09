8aa2fdd409ae7c6bc4b7fcaa97aab16c7aa6a3e4
Christian <criistian-14@hotmail.com>
2024-05-09 18:09:34 -0500

    fix(bitmap): optimization of bitmap when is too large

--- ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/Utils.java
+++ ScanDemoExample/scanlibrary/src/main/java/com/scanlibrary/Utils.java
@@ -2,12 +2,15 @@ package com.scanlibrary;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.util.Log;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -31,6 +34,41 @@ public class Utils {
     }
 
     public static Bitmap getBitmap(Context context, Uri uri) throws IOException {
-        return MediaStore.Images.Media.getBitmap(context.getContentResolver(), uri);
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true; // Get just dimensions
+
+        InputStream input = context.getContentResolver().openInputStream(uri);
+        BitmapFactory.decodeStream(input, null, options);
+        if (input != null) input.close();
+
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        int desiredWidth = displayMetrics.widthPixels / 2;
+        int desiredHeight = displayMetrics.heightPixels / 2;
+
+        options.inSampleSize = calculateInSampleSize(options, desiredWidth, desiredHeight);
+
+        // Calculate bitmap with new size
+        options.inJustDecodeBounds = false;
+        input = context.getContentResolver().openInputStream(uri);
+        Bitmap bitmap = BitmapFactory.decodeStream(input, null, options);
+        if (input != null) input.close();
+        return bitmap;
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 }
